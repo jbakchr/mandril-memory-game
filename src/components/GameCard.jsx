6360e0115143ba3e33775e2fdb249aa06c51aad2
Jonas Bak Phillipson <jbakchr@gmail.com>
2023-01-30 08:35:45 +0100

    Added background image to game card

--- src/components/GameCard.jsx
+++ src/components/GameCard.jsx
@@ -2,12 +2,22 @@ import React from "react";
 import { Paper } from "@mui/material";
 import Grid from "@mui/material/Unstable_Grid2";
 
+import logo from "../assets/card_logo.jpeg";
+
 export const GameCard = () => {
   return (
     <Grid xs={2}>
-      <Paper style={{ height: "150px", borderRadius: "25px" }} elevation={5}>
-        Ost
-      </Paper>
+      <Paper
+        style={{
+          backgroundRepeat: "no-repeat",
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+          height: "150px",
+          borderRadius: "25px",
+          background: `url(${logo})`,
+        }}
+        elevation={5}
+      />
     </Grid>
   );
 };
