ec64312387c483446d1fab2c98685143f93de2ff
Jonas Bak Phillipson <jbakchr@gmail.com>
2023-01-30 07:24:02 +0100

    Added initial game card

---
+++ src/components/GameCard.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import { Grid } from "@mui/material";
+
+export const GameCard = () => {
+  return <Grid xs={1}>Ost</Grid>;
+};
--- src/components/GameGrid.jsx
+++ src/components/GameGrid.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { Box, Grid } from "@mui/material";
 
+import { GameCard } from "./GameCard";
+
 export const GameGrid = () => {
   return (
     <Box sx={{ flexGrow: 1 }}>
       <Grid container spacing={2}>
-        <Grid xs={1}>ost</Grid>
-        <Grid xs={1}>ost</Grid>
+        <GameCard />
+        <GameCard />
       </Grid>
     </Box>
   );
