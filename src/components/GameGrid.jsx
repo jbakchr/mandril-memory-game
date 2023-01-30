5628c8a6484d7a7637a3b2e08731de847c402d9a
Jonas Bak Phillipson <jbakchr@gmail.com>
2023-01-30 09:04:26 +0100

    Added initial game state

--- src/components/GameGrid.jsx
+++ src/components/GameGrid.jsx
@@ -1,10 +1,22 @@
-import React from "react";
+import { useState, useEffect } from "react";
 import { Box } from "@mui/material";
 import Grid from "@mui/material/Unstable_Grid2";
 
 import { GameCard } from "./GameCard";
 
 export const GameGrid = () => {
+  const [gameCards, setGameCards] = useState([]);
+
+  useEffect(() => {
+    let cards = [];
+    for (let i = 0; i < 4; i++) {
+      cards.push({ position: i, character: "Mogens", selected: false });
+    }
+    setGameCards(cards);
+  }, []);
+
+  console.log("game cards:", gameCards);
+
   return (
     <Box sx={{ flexGrow: 1 }}>
       <Grid container spacing={2}>
