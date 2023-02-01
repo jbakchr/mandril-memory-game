1f1b3e6711e9b703037c9b81c9e754725b3236ac
Jonas Bak Phillipson <jbakchr@gmail.com>
2023-02-01 06:54:29 +0100

    Added firebase and hosting

--- src/index.js
+++ src/index.js
@@ -2,8 +2,21 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import { initializeApp } from "firebase/app";
 
 import "./index.module.css";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCTCyazEJ9Y_msrlgZLM3tC4WI-MtdoiOg",
+  authDomain: "mandril-cfb1e.firebaseapp.com",
+  projectId: "mandril-cfb1e",
+  storageBucket: "mandril-cfb1e.appspot.com",
+  messagingSenderId: "1095411548633",
+  appId: "1:1095411548633:web:f8bd0ee73c46a4333759c8",
+  measurementId: "G-CN6B09BF24",
+};
+
+const app = initializeApp(firebaseConfig);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
