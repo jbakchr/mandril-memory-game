6889ed67ecb5bc94a222733c187fa464e6d7f2cf
Jonas Bak Phillipson <jbakchr@gmail.com>
2023-02-01 08:38:18 +0100

    Added initial 'buy me a coffee' button

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -3,11 +3,22 @@ import React from "react";
 import { GameHeader } from "../components/GameHeader";
 import { GameGrid } from "../components/GameGrid";
 
+import bmc from "../assets/bmc-button.png";
+
 export const HomePage = () => {
   return (
     <>
       <GameHeader />
       <GameGrid />
+      <div style={{ display: "flex", justifyContent: "center" }}>
+        <a
+          href="http://buymeacoffee.com/jbakchr"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img style={{ width: "200px" }} src={bmc} alt="buy me a coffee" />
+        </a>
+      </div>
     </>
   );
 };
